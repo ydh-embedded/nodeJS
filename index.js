0dc4bc6a3ba44f9fd0256f0a5fa5ffa4dbab61fa
ydh-embedded <ydh.postkasten@yahoo.com>
2024-10-22 10:08:15 +0200

    dev

---
+++ index.js
@@ -0,0 +1,12 @@
+require = require("esm")(module/*options*/);
+
+module.exports = require ("./app.js");
+
+
+function Moinsen(){
+    let Hey = "Hello World";
+    console.log(Hey)
+}
+
+
+Moinsen()
